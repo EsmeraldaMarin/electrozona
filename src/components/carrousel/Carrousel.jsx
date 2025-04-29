d37097a5bcd6a73c2ce0d4d3d4ea038c0a1dbc42
Esmeralda <esmemarinm03@gmail.com>
2025-04-28 23:22:04 -0300

    styles: carrusel

--- src/components/carrousel/Carrousel.jsx
+++ src/components/carrousel/Carrousel.jsx
@@ -52,11 +52,11 @@ const Carrousel = () => {
                     </div>
                 ))}
             </div>
-            <button className="carousel-control-prev" type="button" data-bs-target="#carousel" data-bs-slide="prev">
+            <button className="carousel-control-prev not_mobile" type="button" data-bs-target="#carousel" data-bs-slide="prev">
                 <span className="carousel-control-prev-icon" aria-hidden="true"></span>
                 <span className="visually-hidden">Previous</span>
             </button>
-            <button className="carousel-control-next" type="button" data-bs-target="#carousel" data-bs-slide="next">
+            <button className="carousel-control-next not_mobile" type="button" data-bs-target="#carousel" data-bs-slide="next">
                 <span className="carousel-control-next-icon" aria-hidden="true"></span>
                 <span className="visually-hidden">Next</span>
             </button>
